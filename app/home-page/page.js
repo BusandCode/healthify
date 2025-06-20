3c2eb5b2b19de0f17897a5ed60b4851d7d4da9d5
Adetokunbo Andrew <Adetokunboandrew2@gmail.com>
2025-06-20 20:00:39 +0100

    home-page

--- app/home-page/page.js
+++ app/home-page/page.js
@@ -4,7 +4,14 @@ import React from 'react'
 const page = () => {
   return (
     <div>
+      {/* Header Component */}
       <Header />
+
+      {/* Hero Section */}
+      <div>
+        <h1 className='font-semibold lg:text-[50px] max-w-[544px] h-[192px] text-[#4203a9]'>Your Path to Affordable Healthcare Starts Here</h1>
+        <p className='text-[#333333] text-[20px] font-medium w-[496px] h-[46px]'>Easily locate providers, view services, and schedule your next visit with confidence</p>
+      </div>
     </div>
   )
 }
