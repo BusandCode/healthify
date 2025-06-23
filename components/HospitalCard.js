90afc6d3ab1da534ebda108909aec04e25c1eda3
Adetokunbo Andrew <Adetokunboandrew2@gmail.com>
2025-06-23 12:08:51 +0100

    healthify

--- components/HospitalCard.js
+++ components/HospitalCard.js
@@ -70,7 +70,7 @@ const hospitals = [
 const HospitalCard = () => {
   return (
     <div className="px-4 lg:px-8 py-6 max-w-7xl mx-auto">
-    <h1 className='font-semibold mb-5 text-[24px] sm:text-[20px] md:text-[30px] lg:text-[40px]
+    <h1 className='font-semibold mb-4 text-[24px] sm:text-[20px] md:text-[30px] lg:text-[40px]
           text-[#4203a9]'>
           Find Healthcare Services</h1>
       {/* Search Bar */}
@@ -94,7 +94,7 @@ const HospitalCard = () => {
         <button className="max-w-[215px] h-[70px] flex items-center 
         justify-center gap-2 bg-[#D9D9D9] border border-gray-300 rounded-[5px] 
         text-[#2C2C2C] hover:bg-gray-100 transition w-full">
-        <SlidersHorizontal size={18} className='text-[#4203a9] cursor-pointer text-[20px] w-[16px] h-[18px]' /> 
+        <SlidersHorizontal size={18} className='text-[#4203a9] text-[20px] w-[16px] h-[18px]' /> 
         Advanced Filters
         </button>
       </div>
@@ -104,26 +104,27 @@ const HospitalCard = () => {
         {hospitals.map((hospital) => (
           <div key={hospital.id} className="bg-white max-w-[330px] h-[616px] 
           rounded-[25px] shadow border border-gray-200 overflow-hidden relative">
-            <div className='max-w-[280px] mx-auto h-[566px] flex flex-col justify-center items-center gap-[18px]'>
+            <div className='max-w-[280px] mx-auto h-[566px] flex flex-col justify-center items-center'>
                 <div className="relative">
               <Image
                 src={hospital.image}
                 alt={hospital.name}
                 width={400}
                 height={250}
-                className="w-[280px] h-[280px] rounded-[15px]"
+                className="max-w-[280px] h-[250px] rounded-[15px]"
               />
               <span className="absolute top-4 left-2 bg-white text-xs px-2 py-1 rounded shadow">
                 {hospital.distance}
               </span>
-              <Bookmark className="absolute top-4 right-2 text-gray-600" size={20} />
+              <Bookmark className="w-[40px] h-[41.59px] absolute top-4 right-2 rounded-[25px] 
+               bg-[#D9D9D9] text-[#0077CC] p-[12px]" size={25} />
             </div>
             <div className="p-4">
               <span className="text-xs text-blue-600 font-medium bg-blue-100 px-2 py-1 rounded">
                 General Services
               </span>
               <h3 className="mt-2 font-medium text-[23px]">{hospital.name}</h3>
-              <p className="text-[20px] text-gray-600 mb-1">
+              <p className="text-sm sm:text-base text-gray-600 mb-1">
                 Constatation fee: <span className="font-medium">{hospital.fee}</span>
               </p>
               <p className="text-sm sm:text-base text-blue-500 underline">{hospital.address}</p>
