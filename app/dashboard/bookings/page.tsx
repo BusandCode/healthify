d0b69760c58c57a89e0a61010012d8a12fc8f586
Andrew Adetokunbo <155444578+BusandCode@users.noreply.github.com>
2025-10-02 20:59:46 +0100

    Update page.tsx

--- app/dashboard/bookings/page.tsx
+++ app/dashboard/bookings/page.tsx
@@ -13,7 +13,7 @@ const page = () => {
       <NavBar />
 
       {/* Main content */}
-      <main className="lg:ml-[170px] pt-[80px] mb-12 lg:mb-0 sm:pt-[100px] flex flex-col gap-[100px]">
+      <main className="lg:ml-[170px] pt-[80px] mb-20 lg:mb-0 sm:pt-[100px] flex flex-col gap-[100px]">
         <Booking />
         <Footer />
       </main>
