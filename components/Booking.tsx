c6af12ae9f5e2d2b3f4b3199d893825f872700bd
Adetokunbo Andrew <Adetokunboandrew2@gmail.com>
2025-10-02 17:30:38 +0100

    update booking page

--- components/Booking.tsx
+++ components/Booking.tsx
@@ -44,21 +44,20 @@ const Booking = () => {
             <h1 className='font-normal lg:text-[20px] text-[18px] mb-4'>
               Consultation Session at <span className='text-[#4203a9] font-semibold'>Ikeja General Hospital</span>
             </h1>
-            <article>
-              <div className='flex lg:flex-row flex-colgap-[15px]'>
+            <article className='flex flex-col items-start gap-3'>
+              <div className='flex lg:flex-row flex-col items-center gap-[15px]'>
                 {/* time */}
-                <div className='flex items-center gap-2'>
+                <div className='flex items-center gap-1'>
                   <Calendar className='w-5 h-5 lg:w-8 lg:h-8 text-[#4203a9]' />
                   <span className='text-gray-700 g:text-[20px] text-[18px]'>January 30th, 2025</span>
                 </div>
-                <div className='flex items-center gap-2'>
+                <div className='flex items-center gap-1'>
                   <Calendar className='w-5 h-5 lg:w-[26px] lg:h-[26px] text-[#4203a9]' />
                   <span className='text-gray-700 g:text-[20px] text-[18px]'>08:00 am</span>
                 </div>
               </div>
-              <div className='flex lg:flex-row flex-col gap-[15px]'>
-                <button className='w-[212px] text-[18px] lg:text-[20px] flex 
-                items-center justify-center h-[44px] p-[10px] bg-[#4203a9] text-white
+              <div className='flex lg:flex-row flex-col items-center gap-[15px]'>
+                <button className='lg:w-[232px] flex items-center text-[18px] lg:text-[20px] h-[44px] p-[10px] bg-[#4203a9] text-white
                  rounded-[5px]'>Reschedule Session</button>
                  <h1 className='text-[#ED0505] text-[18px] lg:text-[20px]'>Cancel</h1>
               </div>
